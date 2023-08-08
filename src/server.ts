f158b03e713d7d5621d02f4eac8f90c4138074a0
Jorge H <jorgehenrrique@live.com>
2023-08-08 20:47:57 -0300

    criado criacao de usuarios

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { v4 as idv4 } from 'uuid';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import { Users, Patient, Doctor } from './types/types';
-import { readJson } from './dataHandler/dataHendler';
+import { readJson, writeJson } from './dataHandler/dataHendler';
 import auth from './middlewares/auth';
 
 dotenv.config();
@@ -56,14 +56,14 @@ app.post('/login', (req, res) => {
 // Listar dados
 // GET: /users
 app.get('/users', auth, (req, res) => {
-  const users: Users[] = readJson('users');
+  const usersList: Users[] = readJson('users');
 
-  if (users.length > 0) {
+  if (usersList.length > 0) {
     const users: { id: string; name: string }[] = [];
 
-    for (const user of users) {
+    for (const user of usersList) {
       if (user.name === 'admin') {
-        return;
+        continue;
       } else {
         const { id, name } = user;
 
@@ -74,4 +74,49 @@ app.get('/users', auth, (req, res) => {
   } else {
     res.status(404).send('Sem usuarios cadastrados');
   }
+});
+
+// Adicionar usuarios
+// POST: /users/add
+app.post('/users/add', auth, (req, res) => {
+  const { name, cnh, rg, pswd } = req.body;
+
+  let newUser: Users;
+
+  // Coalescência nula (?.)
+  if (rg?.trim()) {
+    if (!name.trim()) {
+      return res
+        .status(400)
+        .send('Dados incompletos. Preencha todos os campos');
+    }
+    newUser = {
+      id: idv4(),
+      name: name,
+      rg: rg,
+    };
+  } else if (cnh?.trim()) {
+    if (!name?.trim() || !pswd?.trim()) {
+      return res
+        .status(400)
+        .send('Dados incompletos. Preencha todos os campos');
+    }
+    newUser = {
+      id: idv4(),
+      name: name,
+      cnh: cnh,
+      pswd: pswd,
+    };
+  } else {
+    return res
+      .status(400)
+      .send('Dados inválidos. Forneça informações corretas.');
+  }
+
+  const users: Users[] = readJson('users');
+  users.push(newUser);
+
+  writeJson(users, 'users');
+  console.log('Usuario criado com sucesso');
+  res.status(201).send('Usuario adicionado com sucesso.');
 });
