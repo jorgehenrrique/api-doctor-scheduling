e79a2868bd768f36fd40039d3e264f6717f5e1f2
Jorge H <jorgehenrrique@live.com>
2023-10-06 15:51:32 -0300

    delete query otimizado

--- src/controllers/deleteQuery.ts
+++ src/controllers/deleteQuery.ts
@@ -6,13 +6,15 @@ import { readJson, writeJson } from '../dataHandler/dataHendler';
 export default function deleteQuery(req: any, res: any) {
   let queries: Appointment[] = readJson('queries');
 
-  const currentQuery = queries.find((query) => query.id === req.params.id);
+  const id = req.params.id;
+
+  const currentQuery = queries.find((query) => query.id === id);
 
   if (currentQuery) {
-    queries = queries.filter((query) => query.id !== req.params.id);
+    queries = queries.filter((query) => query.id !== id);
     writeJson(queries, 'queries');
     return res.status(200).send('Consulta deletada com sucesso');
   } else {
-    return res.status(404).send(`Não há dados com o id: ${req.params.id}`);
+    return res.status(404).send(`Não há dados com o id: ${id}`);
   }
 }
